35f2c27a8cf9a6d504851ddb50044a87746ee737
gouravTheDev <chatterjeegouravking@gmail.com>
2022-06-04 15:12:39 +0530

    first

---
+++ coming-soon.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Magic Moments Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+
+    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <style>
+        @font-face {
+            font-family: morelinia;
+            src: url(fonts/morelinia/Morelinia.ttf);
+        }
+
+        body {
+            background-image: url("/images/coming.gif");
+            height: 100%;
+            min-height: 100vh;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            color: #fff;
+            font-family: "ubuntu";
+        }
+
+        .main-heading {
+            font-size: 120px;
+            font-weight: bold;
+            font-family: "morelinia";
+            margin-top: 30px;
+        }
+
+        .sub-heading {
+            font-family: "ubuntu";
+            font-size: 20px;
+            margin-top: 30px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="container pt-0 mt-0">
+        <div class="row mt-4">
+            {{-- <div class="col-8 mx-auto text-center mb-0 pb-0">
+                <img src="/images/landing.gif" alt="" style="width: 40%; height: 80%">
+            </div> --}}
+            <div class="col-md-12 col-sm-12 mt-4 pt-4 text-center">
+                <h1 class="text-center text-white main-heading animate__animated animate__backInUp animation">Coming
+                    Soon!</h1>
+                <h3 class="text-white text-center sub-heading animate__animated animate__backInDown animation mb-4">Our
+                    our tech team is working to launch on 17th July 2022</h3>
+                <a href="/" class="text-white mt-4">Go to home</a>
+            </div>
+        </div>
+        {{-- <h1 class="animate__animated animate__backInLeft animation">An animated element</h1> --}}
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ landing.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Magic Moments Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="mdi/css/materialdesignicons.min.css">
+
+    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <link rel="stylesheet" href="landing.css">
+</head>
+
+<body>
+    <div class="container pt-0 mt-0 pb-4 mb-4">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse animate__animated animate__backInRight animation"
+                id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto text-center navbar-menus pt-3">
+                    <a href="/" class="nav-link mr-2 text-white">
+                        <p class="border-radius">HOME</p>
+                    </a>
+                    <a href="coming-soon.php" class="nav-link mr-2 text-white">
+                        <p class="border-radius">ABOUT US</p>
+                    </a>
+                    <a href="coming-soon.php" class="nav-link mr-2 text-white">
+                        <p class="border-radius">SHOP</p>
+                    </a>
+                    <a href="coming-soon.php" class="nav-link mr-2 text-white">
+                        <p class="border-radius">BLOG</p>
+                    </a>
+                    <a href="coming-soon.php" class="nav-link mr-2 text-white">
+                        <p class="">CONTACT</p>
+                    </a>
+                </ul>
+                <a href="coming-soon.php" class="mr-4" style="color: #fff">
+                    <i class="mdi mdi-magnify"></i>
+                </a>
+                <a href="coming-soon.php" class="mr-4" style="color: #fff">
+                    <i class="mdi mdi-cart"></i>
+                </a>
+                <a href="coming-soon.php" class="btn btn-sm btn-cus mr-2">
+                    Login
+                </a>
+            </div>
+        </nav>
+        <div class="social-section">
+            <a href="" class="btn mr-4 social-icon-btn mb-4" style="color: #fff">
+                <i class="mdi mdi-facebook social-icon"></i>
+            </a> <br>
+            <a href="" class="btn mr-4 social-icon-btn mb-4" style="color: #fff">
+                <i class="mdi mdi-google social-icon"></i>
+            </a> <br>
+            <a href="" class="btn mr-4 social-icon-btn mb-4" style="color: #fff">
+                <i class="mdi mdi-whatsapp social-icon"></i>
+            </a>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-md-6 col-sm-12 mt-4 pt-4">
+                <h1 class="text-center text-white main-heading mt-4 animate__animated animate__backInUp animation">MAGIC
+                    MOMENTS <br>PHOTOGRAPHY</h1>
+                <h3
+                    class="text-white text-center mb-4 sub-heading pb-4 animate__animated animate__backInDown animation">
+                    Begin your moments with us!</h3>
+                <div
+                    class="card-transparent card-cus px-0 py-0 mt-4 shadow animate__animated animate__bounceInLeft animation">
+                    <div class="card-body px-2 py-3">
+                        <h5 class="text-center text-white font-weight-bold">Date Availability Check</h5>
+                        <form class="mt-4">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <select class="form-control" id="">
+                                            <option value="">Event Type</option>
+                                            <option value="">Wedding Reception</option>
+                                            <option value="">Reception</option>
+                                            <option value="">Birthday</option>
+                                            <option value="">Anniversary</option>
+                                            <option value="">puberty ceremony</option>
+                                            <option value="">Portfolio shoot</option>
+                                            <option value="">Family Get Together</option>
+                                            <option value="">Corporate Meetings</option>
+                                            <option value="">Product Shoot</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <input type="date" class="form-control date" placeholder="Pick the Event date">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <input type="number" class="form-control" placeholder="Enter Phone Number">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Enter City">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <textarea name="" id="" cols="30" rows="3" class="form-control" placeholder="Additional Notes if any"></textarea>
+                            </div>
+                            <div class="form-group text-center mt-4 pb-0 mb-0">
+                                <button type="button" class="btn btn-sm">Check</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
