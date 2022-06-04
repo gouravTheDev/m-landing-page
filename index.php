d8d891bd1e0ed37b5f5e54061635b4411a522ee3
gouravTheDev <chatterjeegouravking@gmail.com>
2022-06-04 17:00:19 +0530

    coming soon updated

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Magic Moments Admin</title>
+    <title>Magic Moments Photography</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="mdi/css/materialdesignicons.min.css">
 
